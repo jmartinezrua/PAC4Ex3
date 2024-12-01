c8bed5470ed1d620ac6407cb7b7f1535fc8b2590
jmartinezrua <jmartinezrua@uoc.edu>
2024-12-01 11:29:39 +0100

    Update Student.java
    19

--- src/main/java/edu/uoc/pac4/user/Student.java
+++ src/main/java/edu/uoc/pac4/user/Student.java
@@ -3,26 +3,58 @@ package edu.uoc.pac4.user;
 import edu.uoc.pac4.execution.Execution;
 import java.util.LinkedList;
 
+/**
+ * Represents a student user.
+ */
 public class Student extends User {
+
     private LinkedList<Execution> executions;
 
+    /**
+     * Constructs a new Student with the specified email, name, and surname.
+     *
+     * @param email the email of the student
+     * @param name the name of the student
+     * @param surname the surname of the student
+     * @throws UserException if the email, name, or surname is invalid
+     */
     public Student(String email, String name, String surname) throws UserException {
         super(email, name, surname);
         this.executions = new LinkedList<>();
     }
 
+    /**
+     * Adds an execution to the student's list of executions.
+     *
+     * @param execution the execution to add
+     */
     public void addExecution(Execution execution) {
         this.executions.add(execution);
     }
 
+    /**
+     * Gets the list of executions performed by the student.
+     *
+     * @return the list of executions
+     */
     public LinkedList<Execution> getExecutions() {
         return executions;
     }
 
+    /**
+     * Gets the last execution submitted by the student.
+     *
+     * @return the last execution
+     */
     public Execution getLastExecutionSubmitted() {
         return executions.getLast();
     }
 
+    /**
+     * Returns a string representation of the student.
+     *
+     * @return a string representation of the student
+     */
     @Override
     public String toString() {
         return String.format("[STUDENT] %s %s (%s) Total executions: %d", getName(), getSurname(), getEmail(), executions.size());
