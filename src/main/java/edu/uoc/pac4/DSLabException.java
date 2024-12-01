e9d8815b355af7b2b35ec00e35a86df5948000f2
jmartinezrua <jmartinezrua@uoc.edu>
2024-12-01 11:09:48 +0100

    Update DSLabException.java
    03

--- src/main/java/edu/uoc/pac4/DSLabException.java
+++ src/main/java/edu/uoc/pac4/DSLabException.java
@@ -1,6 +1,10 @@
 package edu.uoc.pac4;
 
+/**
+ * Exception class for DSLab-related errors.
+ */
 public class DSLabException extends Exception {
+
     public static final String INVALID_NAME = "[ERROR] Name cannot be null, empty or blank";
     public static final String INVALID_DESCRIPTION = "[ERROR] Description cannot be null";
     public static final String INVALID_VERSION_MAJOR = "[ERROR] Major version cannot be negative";
@@ -9,6 +13,11 @@ public class DSLabException extends Exception {
     public static final String NO_ENOUGH_CPU = "[ERROR] This server does not have enough CPU";
     public static final String UNIVERSITY_NULL = "[ERROR] University cannot be null";
 
+    /**
+     * Constructs a new DSLabException with the specified detail message.
+     *
+     * @param message the detail message
+     */
     public DSLabException(String message) {
         super(message);
     }
