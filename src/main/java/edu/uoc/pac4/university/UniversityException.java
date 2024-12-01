28d31db372450cee8f353c6c6c10be28b49c3213
jmartinezrua <jmartinezrua@uoc.edu>
2024-12-01 11:26:49 +0100

    Update UniversityException.java
    18

--- src/main/java/edu/uoc/pac4/university/UniversityException.java
+++ src/main/java/edu/uoc/pac4/university/UniversityException.java
@@ -1,13 +1,21 @@
-// File: `src/main/java/edu/uoc/pac4/university/UniversityException.java`
 package edu.uoc.pac4.university;
 
+/**
+ * Exception class for handling university-related errors.
+ */
 public class UniversityException extends Exception {
+
     public static final String INVALID_NAME = "[ERROR] Name cannot be null, empty or blank";
     public static final String INVALID_ABBREVIATION = "[ERROR] Abbreviation has to contain between 2 and 6 uppercase letters";
     public static final String INVALID_FOUNDATION_DATE = "[ERROR] Foundation date cannot be null or in the future";
     public static final String INVALID_ADDRESS = "[ERROR] Address cannot be null";
     public static final String INVALID_WEBSITE = "[ERROR] Website cannot be null and must be a valid URL";
 
+    /**
+     * Constructs a new UniversityException with the specified detail message.
+     *
+     * @param message the detail message
+     */
     public UniversityException(String message) {
         super(message);
     }
