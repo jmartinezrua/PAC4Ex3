221476e5a068078c33ce498910366b1ed6c4858d
jmartinezrua <jmartinezrua@uoc.edu>
2024-12-01 11:31:45 +0100

    Update UserException.java
    21

--- src/main/java/edu/uoc/pac4/user/UserException.java
+++ src/main/java/edu/uoc/pac4/user/UserException.java
@@ -1,9 +1,18 @@
 package edu.uoc.pac4.user;
 
+/**
+ * Exception class for handling user-related errors.
+ */
 public class UserException extends Exception {
-    public static final String INVALID_EMAIL = "[ERROR] The email must start with any character end with @uoc.edu";
+
+    public static final String INVALID_EMAIL = "[ERROR] The email must start with any character and end with @uoc.edu";
     public static final String INVALID_NAME = "[ERROR] Name cannot be null, empty or blank";
 
+    /**
+     * Constructs a new UserException with the specified detail message.
+     *
+     * @param message the detail message
+     */
     public UserException(String message) {
         super(message);
     }
