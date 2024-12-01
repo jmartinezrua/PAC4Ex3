920af7d7c539abf92a276ffaa9b4268241686669
jmartinezrua <jmartinezrua@uoc.edu>
2024-12-01 11:24:54 +0100

    Update Execution.java
    16

--- src/main/java/edu/uoc/pac4/execution/Execution.java
+++ src/main/java/edu/uoc/pac4/execution/Execution.java
@@ -5,11 +5,22 @@ import edu.uoc.pac4.activity.ActivityQuiz;
 import edu.uoc.pac4.user.Student;
 import java.util.Locale;
 
+/**
+ * Represents the execution of an activity by a student.
+ */
 public class Execution {
+
     private double score;
     private Student student;
     private Activity activity;
 
+    /**
+     * Constructs an execution with the specified score, student, and activity.
+     *
+     * @param score the score of the execution
+     * @param student the student who performed the execution
+     * @param activity the activity that was executed
+     */
     public Execution(double score, Student student, Activity activity) {
         setScore(score);
         this.student = student;
@@ -18,10 +29,20 @@ public class Execution {
         this.activity.addExecution(this);
     }
 
+    /**
+     * Gets the score of the execution.
+     *
+     * @return the score of the execution
+     */
     public double getScore() {
         return score;
     }
 
+    /**
+     * Sets the score of the execution. The score is clamped between 0.0 and 10.0.
+     *
+     * @param score the score to set
+     */
     public void setScore(double score) {
         if (score < 0.0) {
             this.score = 0.0;
@@ -32,22 +53,47 @@ public class Execution {
         }
     }
 
+    /**
+     * Gets the student who performed the execution.
+     *
+     * @return the student
+     */
     public Student getStudent() {
         return student;
     }
 
+    /**
+     * Sets the student who performed the execution.
+     *
+     * @param student the student to set
+     */
     public void setStudent(Student student) {
         this.student = student;
     }
 
+    /**
+     * Gets the activity that was executed.
+     *
+     * @return the activity
+     */
     public Activity getActivity() {
         return activity;
     }
 
+    /**
+     * Sets the activity that was executed.
+     *
+     * @param activity the activity to set
+     */
     public void setActivity(Activity activity) {
         this.activity = activity;
     }
 
+    /**
+     * Returns a string representation of the execution.
+     *
+     * @return a string representation of the execution
+     */
     @Override
     public String toString() {
         return String.format(Locale.US, "[STUDENT] %s %s (%s) Total executions: %d%n%s%nTotal executions: %d%n%s%nScore: %.1f",
