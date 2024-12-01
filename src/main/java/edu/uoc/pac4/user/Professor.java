5fb49b07e6d94ebfe10216566e2bdc9653257b5e
jmartinezrua <jmartinezrua@uoc.edu>
2024-12-01 11:27:34 +0100

    Update Professor.java
    19

--- src/main/java/edu/uoc/pac4/user/Professor.java
+++ src/main/java/edu/uoc/pac4/user/Professor.java
@@ -1,25 +1,51 @@
-// Professor.java
 package edu.uoc.pac4.user;
 
 import edu.uoc.pac4.activity.Activity;
 import java.util.LinkedList;
 
+/**
+ * Represents a professor user.
+ */
 public class Professor extends User {
+
     private LinkedList<Activity> activities;
 
+    /**
+     * Constructs a new Professor with the specified email, name, and surname.
+     *
+     * @param email the email of the professor
+     * @param name the name of the professor
+     * @param surname the surname of the professor
+     * @throws UserException if the email, name, or surname is invalid
+     */
     public Professor(String email, String name, String surname) throws UserException {
         super(email, name, surname);
         this.activities = new LinkedList<>();
     }
 
+    /**
+     * Adds an activity to the professor's list of activities.
+     *
+     * @param activity the activity to add
+     */
     public void addActivity(Activity activity) {
         activities.add(activity);
     }
 
+    /**
+     * Gets the list of activities assigned to the professor.
+     *
+     * @return the list of activities
+     */
     public LinkedList<Activity> getActivities() {
         return activities;
     }
 
+    /**
+     * Returns a string representation of the professor.
+     *
+     * @return a string representation of the professor
+     */
     @Override
     public String toString() {
         return String.format("[PROFESSOR] %s %s (%s) Total activities: %d",
