daaabbf89f553689e30655a0e8f1996d21a0b883
jmartinezrua <jmartinezrua@uoc.edu>
2024-12-01 11:30:49 +0100

    Update User.java
    20

--- src/main/java/edu/uoc/pac4/user/User.java
+++ src/main/java/edu/uoc/pac4/user/User.java
@@ -1,20 +1,43 @@
 package edu.uoc.pac4.user;
 
+/**
+ * Represents an abstract user with an email, name, and surname.
+ */
 public abstract class User {
+
     private String email;
     private String name;
     private String surname;
 
+    /**
+     * Constructs a new User with the specified email, name, and surname.
+     *
+     * @param email the email of the user
+     * @param name the name of the user
+     * @param surname the surname of the user
+     * @throws UserException if the email, name, or surname is invalid
+     */
     public User(String email, String name, String surname) throws UserException {
         setEmail(email);
         setName(name);
         setSurname(surname);
     }
 
+    /**
+     * Gets the email of the user.
+     *
+     * @return the email of the user
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Sets the email of the user.
+     *
+     * @param email the email to set
+     * @throws UserException if the email is null or invalid
+     */
     public void setEmail(String email) throws UserException {
         if (email == null || !email.matches("^[^@]+@uoc\\.edu$")) {
             throw new UserException(UserException.INVALID_EMAIL);
@@ -22,10 +45,21 @@ public abstract class User {
         this.email = email;
     }
 
+    /**
+     * Gets the name of the user.
+     *
+     * @return the name of the user
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the user.
+     *
+     * @param name the name to set
+     * @throws UserException if the name is null or empty
+     */
     public void setName(String name) throws UserException {
         if (name == null || name.trim().isEmpty()) {
             throw new UserException(UserException.INVALID_NAME);
@@ -33,14 +67,29 @@ public abstract class User {
         this.name = name.trim();
     }
 
+    /**
+     * Gets the surname of the user.
+     *
+     * @return the surname of the user
+     */
     public String getSurname() {
         return surname;
     }
 
+    /**
+     * Sets the surname of the user.
+     *
+     * @param surname the surname to set
+     */
     public void setSurname(String surname) {
         this.surname = (surname == null) ? "" : surname.trim();
     }
 
+    /**
+     * Returns a string representation of the user.
+     *
+     * @return a string representation of the user
+     */
     @Override
     public String toString() {
         return name + " " + surname + " (" + email + ")";
